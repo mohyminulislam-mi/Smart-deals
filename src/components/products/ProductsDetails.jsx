786d9f26ea53db399b0256280fe2367c89b345d8
Mohyminul Islam <mohyminulmunna23@gmail.com>
2025-11-28 17:38:17 +0600

    Make Product details page

--- src/components/products/ProductsDetails.jsx
+++ src/components/products/ProductsDetails.jsx
@@ -1,7 +1,8 @@
 import React, { use, useEffect, useRef, useState } from "react";
-import { useLoaderData } from "react-router";
+import { Link, useLoaderData } from "react-router";
 import { AuthContext } from "../../context/AuthContext";
 import Swal from "sweetalert2";
+import { FaEnvelope, FaMapMarkerAlt, FaUser } from "react-icons/fa";
 
 const ProductsDetails = () => {
   const { user } = use(AuthContext);
@@ -68,14 +69,78 @@ const ProductsDetails = () => {
       });
   };
   return (
-    <div>
-      <h1>product</h1>
+    <div className="w-11/12 mx-auto">
+      <div className="grid grid-cols-1 md:grid-cols-3">
+        {/* products descriptions */}
+        <div className="col-span-1 mb-6">
+          <img className="h-80 rounded-lg" src="https://images.unsplash.com/photo-1510174210589-ed6667381173?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&q=80&w=1073" alt="" />
+          <h3 className="text-xl font-semibold mb-2">Product Description</h3>
+          <p>
+            <span className="font-semibold">Condition:</span> New
+          </p>
+          <p>
+            <span className="font-semibold">Usage Time:</span> 3 Month
+          </p>
+          <p className="mt-2 text-sm text-gray-600 leading-relaxed">
+            It is a long established fact that a reader will be distracted by
+            the readable content of a page when looking at its layout. The point
+            of using Lorem Ipsum is that it has a more-or-less normal
+            distribution of letters, as opposed to using 'Content here, content
+            here', making it look like readable English. Many desktop publishing
+            packages and web page editors now use Lorem Ipsum as their default
+            model text, and a search for 'lorem Ipsum' will uncover many web
+            sites still in their infancy. Various versions have evolved over the
+            years, sometimes by accident, sometimes on purpose (injected humour
+            and the like).
+          </p>
+        </div>
+        {/* products imformations */}
+        <div className="col-span-2 p-6 border border-base-300">
+          <h2 className="text-2xl font-bold mb-2">Yamaha Fz Guitar For Sale</h2>
+          <p className="badge badge-secondary mb-4">Art And Hobbies</p>
 
-      {/* modal popUp */}
-      <button className="btn" onClick={handleBidModalBox}>
-        open modal
+          <div className="text-lg font-semibold mb-2">
+            Price: <span className="text-primary">$22.5 - $30</span>
+          </div>
+          <p className="text-sm text-gray-500 mb-4">Price starts from</p>
+
+          <div className="grid grid-cols-2 gap-4 mb-6">
+            <div>
+              <p>
+                <span className="font-semibold">Product ID:</span>{" "}
+                687753ae2174ca368ec882f4
+              </p>
+              <p>
+                <span className="font-semibold">Posted:</span> 10/19/2024
+              </p>
+            </div>
+            <div>
+              <p className="flex items-center gap-2">
+                <FaUser /> Sara Chen
+              </p>
+              <p className="flex items-center gap-2">
+                <FaEnvelope /> crafts.by.sara@shop.net
+              </p>
+              <p className="flex items-center gap-2">
+                <FaMapMarkerAlt /> Los Angeles, CA
+              </p>
+              <p>
+                <span className="font-semibold">Contact:</span>{" "}
+                sara.chen_contact
+              </p>
+              <p>
+                <span className="font-semibold">Status:</span>{" "}
+                <span className="text-green-600">On Sale</span>
+              </p>
+            </div>
+          </div>
+          {/* modal popUp */}
+      <button  onClick={handleBidModalBox} className="btn btn-primary w-full text-lg">
+        I Want Buy This Product
       </button>
-
+        </div>
+      </div>
+{/* modal popUp content */}
       <dialog ref={bidModalRef} className="modal modal-bottom sm:modal-middle">
         <div className="modal-box">
           <h3 className="font-bold text-lg text-center">
