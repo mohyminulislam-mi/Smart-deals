e5a2c3b83fd8b5c36ddfeb6060ebecc1419ddb5d
Mohyminul Islam <mohyminulmunna23@gmail.com>
2025-11-28 11:05:14 +0600

    added delete bid from UI when user click deleted

--- src/pages/Bids/MyBids.jsx
+++ src/pages/Bids/MyBids.jsx
@@ -1,5 +1,6 @@
 import React, { use, useEffect, useState } from "react";
 import { AuthContext } from "../../context/AuthContext";
+import Swal from "sweetalert2";
 
 const MyBids = () => {
   const { user } = use(AuthContext);
@@ -15,9 +16,96 @@ const MyBids = () => {
         });
     }
   }, [user?.email]);
+  const handleDeleteBid = (_id) => {
+    Swal.fire({
+      title: "Are you sure?",
+      text: "You won't be able to revert this!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes, delete it!",
+    }).then((result) => {
+      if (result.isConfirmed) {
+        fetch(`http://localhost:3000/bids/${_id}`, {
+          method: "DELETE",
+        })
+          .then((res) => res.json())
+          .then((data) => {
+            console.log("deleted", data);
+
+            if (data.deletedCount) {
+              Swal.fire({
+                title: "Deleted!",
+                text: "Your bid has been deleted.",
+                icon: "success",
+              });
+              const remainingBids = bids.filter((bid) => bid._id !== _id);
+              setBids(remainingBids);
+            }
+          });
+      }
+    });
+  };
   return (
     <div>
-      <h1>MyBids: {bids.length}</h1>
+      <div>
+        <h1>
+          MyBids:
+          {bids.length}
+        </h1>
+      </div>
+
+      <div className="overflow-x-auto">
+        <table className="table">
+          {/* head */}
+          <thead>
+            <tr>
+              <th>SL No.</th>
+              <th>Buyer Name</th>
+              <th>Buyer Email</th>
+              <th>Bid Price</th>
+              <th>Actions</th>
+            </tr>
+          </thead>
+          <tbody>
+            {bids.map((bid, index) => (
+              <tr>
+                <th>{index + 1}1</th>
+                <td>
+                  <div className="flex items-center gap-3">
+                    <div className="avatar">
+                      <div className="mask mask-squircle h-12 w-12">
+                        <img src={bid.buyer_image} alt="Image" />
+                      </div>
+                    </div>
+                    <div>
+                      <div className="font-bold">Dear sam</div>
+                      <div className="text-sm opacity-50">I'm bd</div>
+                    </div>
+                  </div>
+                </td>
+                <td>
+                  {bid.buyer_email}
+                  <br />
+                  <span className="badge badge-ghost badge-sm">
+                    Desktop Support Technician
+                  </span>
+                </td>
+                <td>Price 500</td>
+                <th>
+                  <button
+                    onClick={() => handleDeleteBid(bid._id)}
+                    className="btn btn-outline text-red-500"
+                  >
+                    Remove Bid
+                  </button>
+                </th>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 };
