6c64b696fe55778e34a9345d0f196346bb702cc0
Mohyminul Islam <mohyminulmunna23@gmail.com>
2025-12-02 20:41:51 +0600

    Added axiosSecure and interceptors for more data secures

--- src/components/LatestProducts.jsx
+++ src/components/LatestProducts.jsx
@@ -1,28 +1,24 @@
 import React, { useEffect, useState } from "react";
 import Products from "./products/Products";
-import useAxiosSecure from "../hooks/useAxiosSecure";
 import useAuth from "../hooks/useAuth";
 import Loading from "../Loading/Loading";
 import { Link } from "react-router";
 import { FaArrowRightLong } from "react-icons/fa6";
+import useAxios from "../hooks/useAxios";
 
 const LatestProducts = () => {
-  const {user, loading } = useAuth();
-  console.log(loading);
+  const { user, loading } = useAuth();
   const [products, setProducts] = useState([]);
-  const axiosSecure = useAxiosSecure();
+  const axiosInstance = useAxios();
 
   useEffect(() => {
-    if (user && user.accessToken) {
-       axiosSecure.get("/latest-products").then((data) => {
-      console.log("data", data.data);
-      setProducts(data.data);
+    axiosInstance.get("/latest-products").then((res) => {
+      setProducts(res.data);
     });
-    }
-  }, [user]);
+  }, [user, axiosInstance]);
 
   if (loading) {
-    return <Loading />
+    return <Loading />;
   }
   return (
     <div className="w-11/12 mx-auto">
@@ -35,7 +31,7 @@ const LatestProducts = () => {
         ))}
       </div>
       <div className="text-center my-16">
-        <Link to='/allproduct' className="btn bg-purple-500 text-white ">
+        <Link to="/allproduct" className="btn bg-purple-500 text-white ">
           Show All <FaArrowRightLong />
         </Link>
       </div>
--- src/hooks/useAxiosSecure.jsx
+++ src/hooks/useAxiosSecure.jsx
@@ -1,24 +1,42 @@
 import axios from "axios";
 import useAuth from "./useAuth";
+import { useEffect } from "react";
+import { useNavigate } from "react-router";
 
 const instance = axios.create({
   baseURL: "http://localhost:3000",
 });
 
 const useAxiosSecure = () => {
-  const { user } = useAuth();
+  const { user, singOutUser } = useAuth();
+  const navigate = useNavigate();
 
-  if (!user) {
-    return;
-  }
-  console.log(user);
-  // Add a request interceptor
-  instance.interceptors.request.use((config) => {
-    // Do something before request is sent
-    config.headers.authorization = `Bearer ${user.accessToken}`;
+  useEffect(() => {
+    const requestInterceptor = instance.interceptors.request.use((config) => {
+      const token = user?.accessToken;
+      if (token) {
+        config.headers.authorization = `Bearer ${token}`;
+      }
+      return config;
+    });
 
-    return config;
-  });
+    const responseInterceptor = instance.interceptors.response.use(
+      (res) => res,
+      (err) => {
+        const status = err.response?.status;
+        if (status === 401 || status === 403) {
+          singOutUser().then(() => navigate("/login"));
+        }
+        return Promise.reject(err);
+      }
+    );
+
+    return () => {
+      instance.interceptors.request.eject(requestInterceptor);
+      instance.interceptors.response.eject(responseInterceptor);
+    };
+  }, [user, singOutUser, navigate]);
+
   return instance;
 };
 
